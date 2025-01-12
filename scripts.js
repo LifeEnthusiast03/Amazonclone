99aa16d5bba5f564139f117bb1103d4088cadf21
SOUGATA SAHA <sahasougata820@gmail.com>
2025-01-12 18:04:01 +0530

    add product page ,scripts page ,and some imeges

---
+++ scripts.js
@@ -0,0 +1,39 @@
+let cloths=[
+    {
+        Name:"T-shirt" ,
+        price:399
+    },
+    {
+        Name:"Pant" ,
+        price:399
+    },
+    {
+        Name:"Huddy" ,
+        price:399
+    },
+    {
+        Name:"Jogars" ,
+        price:399
+    },
+    {
+        Name:"Trousars" ,
+        price:399
+    },
+    {
+        Name:"Socks" ,
+        price:399
+    },
+    {
+        Name:"Swater" ,
+        price:399
+    },
+    {
+        Name:"Underware" ,
+        price:399
+    }
+
+]
+let ab=document.getElementsByClassName("procduct-imege")
+ab[0].style.backgroundImage="url(t-shirts.webp";
+
+
